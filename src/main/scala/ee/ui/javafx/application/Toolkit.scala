7629ebe7dadc4a13db65b15e3c8f24661e1ff198
EECOLOR <eecolor@qirx.org>
2013-03-25 22:31:23 +0100

    Removed all, fresh start

--- src/main/scala/ee/ui/javafx/application/Toolkit.scala
+++
@@ -1,7 +0,0 @@
-package ee.ui.javafx.application
-
-import com.sun.javafx.tk.{Toolkit => JavaFxToolkit}
-
-trait Toolkit {
-	def toolkit = JavaFxToolkit.getToolkit
-}
--- src/test/scala/ee/ui/javafx/application/BreakPoint.scala
+++
@@ -1,5 +0,0 @@
-package ee.ui.javafx.application
-
-object BreakPoint { 
-  def set = {}
-}
